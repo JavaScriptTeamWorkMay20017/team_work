d8592ea58dd8a5343dc6a449227dcf6e71913d9a
Todor Stoyanov <t.i.stoianov@abv.bg>
2017-06-17 16:42:35 +0300

    task 7-2 done!

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -261,7 +261,8 @@ function startApp() {
             method: "GET",
             url: kinveyUserUrl,
             headers: kinveyAuthHeaders,
-            success: afterPublisherRequest
+            success: afterPublisherRequest,
+            error: showError
         });
 
         function afterPublisherRequest(publisher) {
@@ -278,11 +279,13 @@ function startApp() {
                 url: kinveyAdvertsUrl,
                 headers: kinveyAuthHeaders,
                 data: advertData,
-                success: createAdvertSuccess
+                success: createAdvertSuccess,
+                error: handleAjaxError
             });
 
             function createAdvertSuccess(response) {
                 listAdverts();
+                showInfo('Advertisement created.');
             }
         }
     }
@@ -299,11 +302,13 @@ function startApp() {
             method: "DELETE",
             url: kinveyBookUrl,
             headers: kinveyAuthHeaders,
-            success: deleteBookSuccess
+            success: deleteBookSuccess,
+            error: handleAjaxError
         });
 
         function deleteBookSuccess(response) {
             listAdverts();
+            showInfo('Advert deleted.');
         }
     }
 
@@ -319,7 +324,8 @@ function startApp() {
             method: "GET",
             url: kinveyBookUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertForEditSuccess
+            success: loadAdvertForEditSuccess,
+            error: handleAjaxError
         });
 
         function loadAdvertForEditSuccess(advert) {
@@ -352,11 +358,13 @@ function startApp() {
             url: kinveyAdvertUrl,
             headers: kinveyAuthHeaders,
             data: advertData,
-            success: editAdvertSuccess
+            success: editAdvertSuccess,
+            error: handleAjaxError
         });
 
         function editAdvertSuccess(response) {
             listAdverts();
+            showInfo('Advertisement edited.');
         }
     }
 }
